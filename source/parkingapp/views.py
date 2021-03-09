3728a9751be73c2ab9870c67d593fd0a6b01cccd
Alexander Thurston <alexanderthurston@gmail.com>
2021-03-08 17:33:42 -0700

    Preliminary placeholders for views.py and urls.py

--- source/parkingapp/views.py
+++ source/parkingapp/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
 
 
+# Homepage
 def index(request):
     return render(request, "parkingapp/index.html")
+
+
+# Login procedures
+def sign_in(request):
+    pass
+
+
+# First time sign-up or updating account info
+def sign_up_or_update_account(request):
+    pass
+
+
+# Account details
+def account(request):
+    pass
+
+
+# Reserve parking spot
+def reserve_spot(request):
+    pass
+
+
+# List lot and spots
+def list_spot(request):
+    pass
+
