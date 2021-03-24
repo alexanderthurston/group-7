1e8187e45d78bc4f878c5d1b081a239d40501448
Alexander Thurston <alexanderthurston@gmail.com>
2021-03-24 16:05:19 -0600

    Finished framework for views.py, urls.py, and templates (In need of styling and backend improvements).

--- source/parkingapp/urls.py
+++ source/parkingapp/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'parkingapp'
 urlpatterns = [
     path('', views.sign_in, name='sign-in'),
-    path('index', views.index, name='index'),
+    path('index/<int:account_id>', views.index, name='index'),
+    path('sign-up', views.sign_up, name="sign-up"),
+    path('find-account', views.find_account, name="find-account"),
     path('create-account', views.create_account, name="create-account"),
     path('update-account/<int:account_id>', views.update_account, name="update-account"),
     path('account-info/<int:account_id>', views.account_info, name="account-info"),
+    path('lot-attendant-home/<account_id>', views.lot_attendant_home, name="lot-attendant-home"),
     path('reserve-spot/<int:account_id>', views.reserve_spot, name="reserve-spot"),
     path('list-lot/<int:account_id>', views.list_lot, name="list-lot"),
     path('manage-lot/<int:account_id>/<int:lot_id>', views.manage_lot, name="manage-lot"),
+    path('supervisor-home/<int:account_id>', views.supervisor_home, name="supervisor-home"),
 ]
--- source/parkingapp/views.py
+++ source/parkingapp/views.py
@@ -2,26 +2,52 @@ from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.utils import timezone
 from django.urls import reverse
 from .models import User, UserType
+
+
 # Homepage
-def index(request):
-    account = User.objects.get(username=request.POST['username'], password=request.POST['password'])
+def index(request, account_id):
+    account = User.objects.get(pk=account_id)
     account.logged_in = True
-    context = {"account": account,}
+    context = {"account": account, }
     return render(request, "parkingapp/index.html", context)
 
 
-# Login procedures
-def sign_in(request):
-    return render(request, "parkingapp/sign_in.html")
+# Sign-in page | will display error message upon completion of view
+def sign_in(request, error_message=""):
+    context = {'error_message': error_message}
+    return render(request, "parkingapp/sign_in.html", context)
 
 
 # First time sign-up
+def sign_up(request):
+    return render(request, "parkingapp/sign_up.html")
+
+
+# Create user account in system
 def create_account(request):
     pass
 
+
+# Find and verify user's account in system
+def find_account(request):
+    # try:
+    #     account = User.objects.get(username=request.POST['username'], password=request.POST['password'])
+    #     return HttpResponseRedirect(reverse('parkingapp:index', args=[account.id]))
+    # except(KeyError, User.DoesNotExist):   # Redirect user and alert them of invalid sign-in
+    #     error_message = "Invalid sign-in credentials. Please Try Again"
+    #
+    #     # return HttpResponseRedirect(reverse('parkingapp:sign-in', args=[error_message]))
+
+    account = User.objects.get(username=request.POST['username'], password=request.POST['password'])
+    return HttpResponseRedirect(reverse('parkingapp:index', args=[account.id]))
+
+
 # Update account info
-def update_account(request):
-    pass
+def update_account(request, account_id):
+    account = User.objects.get(pk=account_id)
+    context = {'account': account, }
+    return render(request, "parkingapp/account_info.html", context)
+
 
 # Account details
 def account_info(request, account_id):
@@ -30,20 +56,49 @@ def account_info(request, account_id):
     return render(request, "parkingapp/account_info.html", context)
 
 
-
 # Reserve parking spot
-def reserve_spot(request):
-    pass
+def reserve_spot(request, account_id):
+    account = User.objects.get(pk=account_id)
+    context = {'account': account, }
+    return render(request, "parkingapp/reserve_spot.html", context)
 
 
 # List lot and spots
-def list_lot(request):
-    pass
+def list_lot(request, account_id):
+    account = User.objects.get(pk=account_id)
+    context = {'account': account, }
+    return render(request, "parkingapp/list_lot.html", context)
+
 
 # Manage lot
-def manage_lot(request):
+def manage_lot(request, account_id, lot_id):
+    account = User.objects.get(pk=account_id)
+    context = {'account': account, }
+    return render(request, "parkingapp/manage_lot.html", context)
+
+# Verification portal
+def lot_attendant_home(request, account_id):
+    account = User.objects.get(pk=account_id)
+    context = {'account': account, }
+    return render(request, "parkingapp/lot_attendant_home.html", context)
+
+
+# Verification confirmation
+def lot_attendant_confirmation(request):
     pass
 
-# Supervisor overview
-def overview(request):
+# Supervisor view to create event
+def create_event(request):
     pass
+
+
+# Supervisor view to manage already listed events
+def manage_event(request):
+    pass
+
+
+# Supervisor overview
+def supervisor_home(request, account_id):
+    account = User.objects.get(pk=account_id)
+    context = {'account': account, }
+    return render(request, "parkingapp/supervisor_home.html", context)
