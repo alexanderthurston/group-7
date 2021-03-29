a1cb85fb9c50adec1a1a64c3eab8480c830089f7
Jay Peterson <jay.peterson@aggiemail.usu.edu>
2021-03-29 17:01:39 -0600

    linux is bad

--- source/parkingapp/admin.py
+++ source/parkingapp/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-# from .models import User,UserType,Event,ParkingLot,ParkingSpot,ParkingSpotType,ParkingSpotHistory,TransactionHistory,TransactionType
+from .models import User,UserType,Event,ParkingLot,ParkingSpot,ParkingSpotType,ParkingSpotHistory,TransactionHistory,TransactionType
 
-# admin.site.register(User)
+admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(ParkingLot)
+admin.site.register(ParkingSpotHistory)
+admin.site.register(TransactionHistory)
+
+
--- source/parkingapp/models.py
+++ source/parkingapp/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     lastName = models.CharField(max_length=50)
     email = models.EmailField(default="")
     phoneNumber = models.IntegerField(default=0)
-    balance = models.IntegerField(default=0)
+    balance = models.DecimalField(decimal_places=2, max_digits=5)
     logged_in = models.BooleanField(default=False)
 
     def __str__(self):
@@ -19,9 +19,6 @@ class UserType(models.Model):
     userID = models.ForeignKey(User, on_delete=models.CASCADE)
     typeName = models.CharField(max_length=10)
 
-    # def __str__(self):
-    #     return self.typeName
-    
 
 class Event(models.Model):
     userID = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -29,38 +26,26 @@ class Event(models.Model):
     address = models.CharField(max_length=100)
     eventDate = models.DateTimeField()
 
-    # def __str__(self):
-    #     return self.eventName + ' ' + self.eventDate
+    def __str__(self):
+        return self.eventName + " " + self.eventDate
 
 class ParkingLot(models.Model):
     eventID = models.ForeignKey(Event, on_delete=models.CASCADE)
     address = models.CharField(max_length=200)
     
-    # def __str__(self):
-    #     return self.address
 
 class ParkingSpot(models.Model):
     parkingLotID = models.ForeignKey(ParkingLot, on_delete=models.CASCADE)
     spaceCode = models.CharField(max_length=50)
 
-    # def __str__(self):
-    #     return self.spaceCode
-
 class ParkingSpotType(models.Model):
     parkingSpotID = models.ForeignKey(ParkingSpot, on_delete=models.CASCADE)
     typeName = models.CharField(max_length=20)
-
-    # def __str__(self):
-    #     return self.typeName
 
 class ParkingSpotHistory(models.Model):
     spotID = models.ForeignKey(ParkingSpot, on_delete=models.CASCADE)
     rentStartDate = models.DateTimeField()
     rentEndDate = models.DateTimeField()
-
-    # def __str__(self):
-    #     return self.rentStartDate + ' ' + self.rentEndDate
-    
 
 class TransactionHistory(models.Model):
     userID = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -69,14 +54,6 @@ class TransactionHistory(models.Model):
     transDate = models.DateTimeField()
     amount = models.DecimalField(decimal_places=2, max_digits=5)
 
-    # def __str__(self):
-    #     return self.userID + ' ' + self.eventID + ' ' + self.transDate
-    
-
 class TransactionType(models.Model):
     transHistoryID = models.ForeignKey(TransactionHistory, on_delete=models.CASCADE)
     transTypeName = models.CharField(max_length=20) 
-
-    # def __str__(self):
-    #     return self.transTypeName
-    
