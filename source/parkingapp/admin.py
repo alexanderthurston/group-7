7f5e55236dbd80f2ba4a30e0edbc025d899e9ff9
Jay Peterson <jay.peterson@aggiemail.usu.edu>
2021-04-07 13:56:21 -0600

    nothing interesting

--- source/parkingapp/admin.py
+++ source/parkingapp/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from .models import UserProfile,UserType,Event,ParkingLot,ParkingSpot,ParkingSpotType,ParkingSpotHistory,TransactionHistory,TransactionType
+# from .models import Profile,UserType,Event,ParkingLot,ParkingSpot,ParkingSpotType,ParkingSpotHistory,TransactionHistory,TransactionType
 
-admin.site.register(UserProfile)
-admin.site.register(Event)
-admin.site.register(ParkingLot)
-admin.site.register(ParkingSpotHistory)
-admin.site.register(TransactionHistory)
+# admin.site.register(Profile)
+# admin.site.register(Event)
+# admin.site.register(ParkingLot)
+# admin.site.register(ParkingSpotHistory)
+# admin.site.register(TransactionHistory)
 
 
